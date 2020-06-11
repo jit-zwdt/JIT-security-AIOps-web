87bb76d90ce30920595b3557a6214736039e7912
frank.ren <junming_ren@jit.com.cn>
2020-06-11 15:07:00 +0800

    增加token刷新功能

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,7 +1,25 @@
 import axios from 'axios'
 import store from '@/store'
 import router from '@/router'
-
+import api from '@/api/api'
+import * as common from '@/utils/common'
+import qs from 'qs'
+import { Message } from 'element-ui'
+// 是否正在刷新的标记
+let isRefreshing = false
+// 重试队列，每一项将是一个待执行的函数形式
+let requests = []
+function getRefreshToken () {
+  const refreshForm = {
+    refresh_token: sessionStorage.getItem('refreshToken')
+  }
+  return refreshForm
+}
+function refreshToken (params) {
+  return axios.post(api.refreshToken, qs.stringify({
+    refresh_token: params.refresh_token
+  })).then(res => res.data)
+}
 axios.interceptors.request.use(
   config => {
     if (sessionStorage.getItem('token')) {
@@ -16,6 +34,49 @@ axios.interceptors.request.use(
 )
 axios.interceptors.response.use(
   response => {
+    const code = response.data.code
+    if (code === 5007) {
+      const config = response.config
+      if (!isRefreshing) {
+        isRefreshing = true
+        const param = getRefreshToken()
+        common.clearToken()
+        return refreshToken(param).then(res => {
+          const token = res.data.access_token
+          const refreshToken = res.data.refresh_token
+          common.setToken(token, refreshToken)
+          config.headers.Authorization = token
+          requests.forEach(cb => cb(token))
+          requests = []
+          return axios(config)
+        }).catch(error => {
+          console.log(error)
+          window.location.href = '/'
+        }).finally(() => {
+          isRefreshing = false
+        })
+      } else {
+        return new Promise((resolve) => {
+          // 将resolve放进队列，用一个函数形式来保存，等token刷新后直接执行
+          requests.push((token) => {
+            resolve(axios(config))
+          })
+        })
+      }
+    } else if (code === 5004) {
+      Message({
+        showClose: true,
+        message: '登录信息过期，请重新登录',
+        type: 'error',
+        duration: 5000
+      })
+      store.commit('LOGOUT')
+      router.currentRoute.path !== 'login' &&
+        router.replace({
+          path: 'login',
+          query: { redirect: router.currentRoute.path }
+        })
+    }
     return response
   },
   error => {
