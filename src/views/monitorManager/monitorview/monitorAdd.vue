0c5dd90804263e0febb861f5f787fd73842f95cc
jian_liu_ggzw <1115218686@qq.com>
2020-06-29 15:41:53 +0800

    更改内容：1、修改监控添加校验 更改人： jian_liu 更改时间：2020年6月29日15点41分

--- src/views/monitorManager/monitorview/monitorAdd.vue
+++ src/views/monitorManager/monitorview/monitorAdd.vue
@@ -13,6 +13,7 @@
           class="edit-forms fromadd"
           label-position="right"
           label-width="150px"
+          :rules="editFormRules"
         >
           <el-form-item label="对象名称" prop="objectName">
             <el-input v-model="serverListForm.objectName" clearable></el-input>
@@ -158,14 +159,14 @@
           <el-form-item label prop="enableMonitor" class="el-form-item-radio">
             <el-checkbox v-model="serverListForm.enableMonitor" value="1">启动监控</el-checkbox>
           </el-form-item>
-          <el-form-item label="(主)模板表ID" prop="typeId" v-if="show">
+          <el-form-item label="模板表ID" prop="templatesId" v-if="show">
+            <el-input v-model="serverListForm.templatesId" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="(主)类型" prop="typeId" v-if="show">
             <el-input v-model="serverListForm.typeId" clearable></el-input>
           </el-form-item>
-          <el-form-item label="(子)类型" prop="subtypeId" v-if="show">
-            <el-input v-model="serverListForm.subtypeId" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="类型" prop="subtypeIds">
-            <el-select v-model="serverListForm.subtypeIds" placeholder="请选择">
+          <el-form-item label="类型" prop="subtypeId">
+            <el-select v-model="serverListForm.subtypeId" placeholder="请选择">
               <el-option
                 v-for="item in subtypeIdOptions"
                 :key="item.id"
@@ -190,6 +191,16 @@
                 :value="template.value"
               ></el-option>
             </el-select>
+          </el-form-item>
+          <el-form-item label="资产" prop="assetsId">
+            <el-cascader
+              :options="assetOptions"
+              :show-all-levels="false"
+              v-model="serverListForm.assetsId"
+              :props="assetProps"
+              clearable
+              class="cascadercss"
+            ></el-cascader>
           </el-form-item>
           <el-form-item label="备注" prop="remark">
             <el-input v-model="serverListForm.remark" clearable></el-input>
@@ -229,6 +240,8 @@ export default {
         proxyMonitor: '',
         enableMonitor: true,
         subtypeId: '',
+        templatesId: '',
+        typeId: '',
         groupId: [],
         remark: '',
         label: '',
@@ -254,13 +267,14 @@ export default {
         vmMacroCpuFrequency: '2666000000',
         vmMacroPassword: '',
         vmMacroSdkLink: 'https://',
-        vmMacroUsername: ''
+        vmMacroUsername: '',
+        assetsId: ''
       },
       tableDataclear: [],
       setTimeoutster: '',
       proxyMonitorOptions: [
         {
-          value: '0',
+          value: '',
           label: '(No Proxy)'
         }
       ],
@@ -279,13 +293,86 @@ export default {
           value: '34',
           label: '数据库模板'
         }
-      ]
+      ],
+      assetOptions: [],
+      assetProps: {
+        value: 'id',
+        label: 'name',
+        children: 'items'
+      },
+      editFormRules: {
+        objectName: [{
+          required: true, // 默认是否显示校验
+          message: '请输入对象名称',
+          trigger: 'blur'
+        }],
+        businessName: [{
+          required: true,
+          message: '请输入业务名称',
+          trigger: 'blur'
+        }],
+        subtypeId: [{
+          required: true,
+          message: '请选择类型',
+          trigger: 'blur'
+        }],
+        groupId: [{
+          required: true,
+          message: '请选择分组',
+          trigger: 'blur'
+        }],
+        agentIp: [{
+          required: true,
+          message: '请输入IP',
+          trigger: 'blur'
+        }],
+        agentDnsName: [{
+          required: true,
+          message: '请输入DNS',
+          trigger: 'blur'
+        }],
+        agentPort: [{
+          required: true,
+          message: '请输入端口',
+          trigger: 'blur'
+        }],
+        jmxIp: [{
+          required: true,
+          message: '请输入IP',
+          trigger: 'blur'
+        }],
+        jmxDnsName: [{
+          required: true,
+          message: '请输入DNS',
+          trigger: 'blur'
+        }],
+        jmxPort: [{
+          required: true,
+          message: '请输入端口',
+          trigger: 'blur'
+        }],
+        snmpIp: [{
+          required: true,
+          message: '请输入IP',
+          trigger: 'blur'
+        }],
+        snmpDnsName: [{
+          required: true,
+          message: '请输入DNS',
+          trigger: 'blur'
+        }],
+        snmpPort: [{
+          required: true,
+          message: '请输入端口',
+          trigger: 'blur'
+        }]
+      }
     }
   },
   created () {
-    this.serverListForm.typeId = this.$route.query.templateId
+    this.serverListForm.templatesId = this.$route.query.templateId
     var templateId = this.$route.query.templateId
-    this.serverListForm.subtypeId = this.$route.query.templateSubTypeId
+    this.serverListForm.typeId = this.$route.query.templateSubTypeId
     var templateSubTypeId = this.$route.query.templateSubTypeId
     if (templateSubTypeId === '10') {
       this.sqlserverShow = true
@@ -314,13 +401,17 @@ export default {
       const _this = this
       this.setTimeoutster = window.setTimeout(() => { _this.showInfoTimeout() }, 300)
     },
-    showInfoTimeout (str) {
+    showInfoTimeout () {
+      this.getSubtypeIdOptions()
+      this.getAssetInfo()
+    },
+    getSubtypeIdOptions () {
       const param = new URLSearchParams()
-      param.append('ids', this.serverListForm.subtypeId)
+      param.append('ids', this.serverListForm.typeId)
       this.axios.post('/monitorType/getJsonTypes', param).then((resp) => {
         if (resp.status === 200) {
           var json = resp.data
-          console.log(json.data)
+          // console.log(json.data)
           if (json.code === 1) {
             this.subtypeIdOptions = json.data
           }
@@ -332,7 +423,24 @@ export default {
         }
       })
     },
+    getAssetInfo () {
+      this.axios.post('/assets/findByConditionInfo').then((resp) => {
+        if (resp.status === 200) {
+          var json = resp.data
+          console.log(json.data)
+          if (json.code === 1) {
+            this.assetOptions = json.data
+          }
+        } else {
+          this.$message({
+            message: '查询失败',
+            type: 'error'
+          })
+        }
+      })
+    },
     submitOrUpdate (formName) {
+      this.checkform()
       this.$refs[formName].validate((valid) => {
         if (valid) {
           this.submit()
@@ -353,6 +461,11 @@ export default {
             })
             this.clearform()
             this.$router.push({ name: 'monitorList' })
+          } else {
+            this.$message({
+              message: '添加失败',
+              type: 'error'
+            })
           }
         } else {
           this.$message({
@@ -363,7 +476,39 @@ export default {
         }
       })
     },
+    checkform () {
+      var templateId = this.$route.query.templateId
+      var templateSubTypeId = this.$route.query.templateSubTypeId
+      if (templateId === '12' || templateId === '13' || templateId === '14') {
+        if (this.serverListForm.snmpType === '1') {
+          this.editFormRules.snmpIp[0].required = true
+          this.editFormRules.snmpDnsName[0].required = false
+        } else {
+          this.editFormRules.snmpIp[0].required = false
+          this.editFormRules.snmpDnsName[0].required = true
+        }
+      } else {
+        if (this.serverListForm.agentType === '1') {
+          this.editFormRules.agentIp[0].required = true
+          this.editFormRules.agentDnsName[0].required = false
+        } else {
+          this.editFormRules.agentIp[0].required = false
+          this.editFormRules.agentDnsName[0].required = true
+        }
+      }
+      if (templateSubTypeId === '13' || templateSubTypeId === '14') {
+        if (this.serverListForm.jmxType === '1') {
+          this.editFormRules.jmxIp[0].required = true
+          this.editFormRules.jmxDnsName[0].required = false
+        } else {
+          this.editFormRules.jmxIp[0].required = false
+          this.editFormRules.jmxDnsName[0].required = true
+        }
+      }
+    },
     makeParam () {
+      var assetsId = (this.serverListForm.assetsId).toString()
+      assetsId = assetsId.split(',')[1]
       var enableMonitor = this.serverListForm.enableMonitor
       if (enableMonitor) {
         enableMonitor = '1'
@@ -383,7 +528,32 @@ export default {
         groupId: (this.serverListForm.groupId).toString(),
         remark: this.serverListForm.remark,
         label: this.serverListForm.label,
-        typeId: this.serverListForm.typeId
+        templatesId: this.serverListForm.templatesId,
+        typeId: this.serverListForm.typeId,
+        mssqlMacroInstance: this.serverListForm.mssqlMacroInstance,
+        mssqlMacroOdbc: this.serverListForm.mssqlMacroOdbc,
+        mssqlMacroPassword: this.serverListForm.mssqlMacroPassword,
+        mssqlMacroUsername: this.serverListForm.mssqlMacroUsername,
+        oracleMacroIp: this.serverListForm.oracleMacroIp,
+        oracleMacroAsm: this.serverListForm.oracleMacroAsm,
+        oracleMacroDbname: this.serverListForm.oracleMacroDbname,
+        oracleMacroPassword: this.serverListForm.oracleMacroPassword,
+        oracleMacroUsername: this.serverListForm.oracleMacroUsername,
+        jmxType: this.serverListForm.jmxType,
+        jmxIp: this.serverListForm.jmxIp,
+        jmxDnsName: this.serverListForm.jmxDnsName,
+        jmxPort: this.serverListForm.jmxPort,
+        jmxMacro: this.serverListForm.jmxMacro,
+        snmpType: this.serverListForm.snmpType,
+        snmpIp: this.serverListForm.snmpIp,
+        snmpDnsName: this.serverListForm.snmpDnsName,
+        snmpPort: this.serverListForm.snmpPort,
+        snmpMacro: this.serverListForm.snmpMacro,
+        vmMacroCpuFrequency: this.serverListForm.vmMacroCpuFrequency,
+        vmMacroPassword: this.serverListForm.vmMacroPassword,
+        vmMacroSdkLink: this.serverListForm.vmMacroSdkLink,
+        vmMacroUsername: this.serverListForm.vmMacroUsername,
+        assetsId: assetsId
       }
       return region
     },
@@ -400,6 +570,30 @@ export default {
       this.serverListForm.groupId = []
       this.serverListForm.remark = ''
       this.serverListForm.label = ''
+      this.mssqlMacroInstance = 'MSSQLSERVER'
+      this.mssqlMacroOdbc = ''
+      this.mssqlMacroPassword = ''
+      this.mssqlMacroUsername = ''
+      this.oracleMacroIp = ''
+      this.oracleMacroAsm = ''
+      this.oracleMacroDbname = 'orcl'
+      this.oracleMacroPassword = ''
+      this.oracleMacroUsername = ''
+      this.jmxType = '1'
+      this.jmxIp = ''
+      this.jmxDnsName = ''
+      this.jmxPort = '12345'
+      this.jmxMacro = ''
+      this.snmpType = '1'
+      this.snmpIp = ''
+      this.snmpDnsName = ''
+      this.snmpPort = '161'
+      this.snmpMacro = ''
+      this.vmMacroCpuFrequency = '2666000000'
+      this.vmMacroPassword = ''
+      this.vmMacroSdkLink = 'https://'
+      this.vmMacroUsername = ''
+      this.serverListForm.assetsId = ''
     },
     backfrom () {
       this.$router.go(-1) // 返回上一层
@@ -461,5 +655,8 @@ export default {
 }
 /deep/.inputcolor input {
   background-color: rgb(232, 240, 254) !important;
+}
+.cascadercss {
+  min-width: 843.594px;
 }
 </style>
