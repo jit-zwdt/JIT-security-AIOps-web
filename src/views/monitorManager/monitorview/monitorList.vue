e8bc3949b06c5daa5c42bb9886316e10024b03d3
jian_liu_ggzw <1115218686@qq.com>
2020-06-30 16:05:16 +0800

    更改内容：1、修改监控列表 更改人： jian_liu 更改时间：2020年6月30日16点05分

--- src/views/monitorManager/monitorview/monitorList.vue
+++ src/views/monitorManager/monitorview/monitorList.vue
@@ -15,7 +15,14 @@
           <el-input type="text" v-model="hostIp" size="small" placeholder="IP" clearable></el-input>
         </el-col>
         <el-col :span="2">
-          <el-select v-model="hostType" class="datetop" filterable placeholder="选择类型" @change="current_hostType" clearable>
+          <el-select
+            v-model="hostType"
+            class="datetop"
+            filterable
+            placeholder="选择类型"
+            @change="current_hostType"
+            clearable
+          >
             <el-option
               v-for="item in hostTypeOptions"
               :key="item.id"
@@ -25,7 +32,14 @@
           </el-select>
         </el-col>
         <el-col :span="2">
-          <el-select v-model="hostGroup" class="datetop" filterable placeholder="选择分组" @change="current_hostGroup" clearable>
+          <el-select
+            v-model="hostGroup"
+            class="datetop"
+            filterable
+            placeholder="选择分组"
+            @change="current_hostGroup"
+            clearable
+          >
             <el-option
               v-for="item in hostGroupOptions"
               :key="item.groupid"
@@ -35,7 +49,14 @@
           </el-select>
         </el-col>
         <el-col :span="2">
-          <el-select v-model="hostSubType" class="datetop" filterable placeholder="选择子类型" @change="current_hostSubType" clearable>
+          <el-select
+            v-model="hostSubType"
+            class="datetop"
+            filterable
+            placeholder="选择子类型"
+            @change="current_hostSubType"
+            clearable
+          >
             <el-option
               v-for="item in hostSubTypeOptions"
               :key="item.id"
@@ -98,29 +119,52 @@
     >
       >
       <el-table-column label="id" prop="id" :resizable="false" v-if="show"></el-table-column>
+      <el-table-column label="zabbix中主机的Hostid" prop="hostid" v-if="show"></el-table-column>
       <el-table-column label="主机名称" prop="objectName" min-width="14%">
         <template slot-scope="scope">
           <el-link type="primary" @click="showAssetsInfo(scope.row)">{{scope.row.objectName}}</el-link>
         </template>
       </el-table-column>
-      <el-table-column label="业务名称" prop="businessName" min-width="14%">
-      </el-table-column>
+      <el-table-column label="业务名称" prop="businessName" min-width="14%"></el-table-column>
       <el-table-column label="IP" prop="hostIp" min-width="12%" :resizable="false"></el-table-column>
       <el-table-column align="center" label="启用监控" min-width="6%" :resizable="false">
         <template slot-scope="scope">
           <el-switch
-                  v-model="scope.row.enableMonitor"
-                  active-value="1"
-                  inactive-value="0"
-                  active-color="#13ce66"
-                  @change="change_enableMonitor(scope.row)"/>
+            v-model="scope.row.enableMonitor"
+            active-value="1"
+            inactive-value="0"
+            active-color="#13ce66"
+            @change="change_enableMonitor(scope.row)"
+          />
         </template>
       </el-table-column>
-      <el-table-column label="监控状态" prop="monitorType" min-width="14%" :resizable="false"></el-table-column>
+      <el-table-column label="监控状态" prop="monitorType" min-width="14%" :resizable="false">
+        <template slot-scope="scope">
+          <el-tag
+            v-for="item in makeMonitorTypeItems(scope.row)"
+            :key="item.label"
+            :type="item.type"
+            effect="dark"
+            :title="item.title"
+            :hit="true"
+          >{{ item.label }}</el-tag>
+        </template>
+      </el-table-column>
       <el-table-column label="类型" prop="type" min-width="6%" :resizable="false"></el-table-column>
       <el-table-column label="子类型" prop="subtype" min-width="6%" :resizable="false"></el-table-column>
       <el-table-column label="备注" prop="remark" min-width="12%" :resizable="false"></el-table-column>
-      <el-table-column label="分组" prop="groupId" min-width="6%" :resizable="false"></el-table-column>
+      <el-table-column label="分组" prop="groupId" min-width="6%" :resizable="false">
+        <template slot-scope="scope">
+          <el-popover trigger="click" placement="top">
+            <p
+              style="width:200px;white-space: pre-line"
+            >{{groupIdformatType(scope.$index, scope.row)}}</p>
+            <div slot="reference" class="name-wrapper">
+              <el-tag size="medium">查看分组</el-tag>
+            </div>
+          </el-popover>
+        </template>
+      </el-table-column>
       <!--<el-table-column label="标签" prop="hostLabel" min-width="6%" :resizable="false"></el-table-column>-->
       <el-table-column align="center" label="操作" min-width="10%">
         <template slot-scope="scope">
@@ -149,7 +193,6 @@ export default {
     return {
       show: false,
       hostObjectName: '',
-      radioHostIp: '',
       hostIp: '',
       hostType: '',
       hostGroup: '',
@@ -168,7 +211,8 @@ export default {
         remark: '',
         groupId: '',
         hostLabel: '',
-        businessName: ''
+        businessName: '',
+        hostid: ''
       }],
       currentPage: 1,
       pageSize: 15,
@@ -198,7 +242,9 @@ export default {
       currentHostType: '',
       currentHostSubType: '',
       currentEnableMonitor: '',
-      currentHostGroup: ''
+      currentHostGroup: '',
+      monitorTypeItems: [],
+      timer: ''
     }
   },
   created () {
@@ -274,6 +320,7 @@ export default {
             this.tableData = json.data.dataList
             this.currentTotal = json.data.totalRow
             this.loading = false
+            this.getMonitorTypeItems()
           }
         } else {
           this.$message({
@@ -283,11 +330,43 @@ export default {
         }
       })
     },
+    groupIdformatType (index, row) {
+      var data = row.groupId
+      var dataname = ''
+      if (data !== undefined && data !== null) {
+        const datalen = data.split(',').length
+        const splitdata = data.split(',')
+        for (var i = 0; i < datalen; i++) {
+          var datachildren = splitdata[i]
+          this.hostGroupOptions.forEach(element => {
+            if (datachildren === element.groupid) {
+              if (datalen > 1) {
+                if (i === 0) {
+                  dataname = element.name
+                } else {
+                  dataname = dataname + '\n' + element.name
+                }
+              } else {
+                dataname = element.name
+                return ''
+              }
+            }
+          })
+        }
+      }
+      return dataname
+    },
     showClear () {
-      this.assetNameTop = ''
-      this.assetRegisterDateStartTop = ''
-      this.assetRegisterDateEndTop = ''
-      this.titleType = ''
+      this.hostObjectName = ''
+      this.hostIp = ''
+      this.hostType = ''
+      this.hostGroup = ''
+      this.hostSubType = ''
+      this.enableMonitor = ''
+      this.currentHostType = ''
+      this.currentHostSubType = ''
+      this.currentEnableMonitor = ''
+      this.currentHostGroup = ''
     },
     showAssetsAdd () {
       // this.$router.push({ name: 'monitorAddList', query: { id: this.hostObjectName } })
@@ -338,6 +417,78 @@ export default {
         }
       })
     },
+    makeMonitorTypeItems (row) {
+      var makehostIds = [
+        { type: 'info', label: 'AGENT', title: '' },
+        { type: 'info', label: 'SNMP', title: '' },
+        { type: 'info', label: 'JMX', title: '' },
+        { type: 'info', label: 'IPMI', title: '' }]
+      this.monitorTypeItems.forEach(element => {
+        if (element.hostid === row.hostid) {
+          var agenttype = ''
+          if (element.available === 0) {
+            agenttype = 'info'
+          } else if (element.available === 1) {
+            agenttype = 'success'
+          } else if (element.available === 2) {
+            agenttype = 'danger'
+          }
+          var snmptype = ''
+          if (element.snmp_available === 0) {
+            snmptype = 'info'
+          } else if (element.snmp_available === 1) {
+            snmptype = 'success'
+          } else if (element.snmp_available === 2) {
+            snmptype = 'danger'
+          }
+          var jmxtype = ''
+          if (element.jmx_available === 0) {
+            jmxtype = 'info'
+          } else if (element.jmx_available === 1) {
+            jmxtype = 'success'
+          } else if (element.jmx_available === 2) {
+            jmxtype = 'danger'
+          }
+          var ipmitype = ''
+          if (element.ipmi_available === 0) {
+            ipmitype = 'info'
+          } else if (element.ipmi_available === 1) {
+            ipmitype = 'success'
+          } else if (element.ipmi_available === 2) {
+            ipmitype = 'danger'
+          }
+          makehostIds[0].type = agenttype
+          makehostIds[0].title = element.error
+          makehostIds[1].type = snmptype
+          makehostIds[1].title = element.snmp_error
+          makehostIds[2].type = jmxtype
+          makehostIds[2].title = element.jmx_error
+          makehostIds[3].type = ipmitype
+          makehostIds[3].title = element.ipmi_error
+          return makehostIds
+        }
+      })
+      return makehostIds
+    },
+    getMonitorTypeItems () {
+      const hostIds = []
+      this.tableData.forEach(element => {
+        hostIds.push(element.hostid)
+      })
+      this.axios.post('/host/findHostAvailable', hostIds).then((resp) => {
+        if (resp.status === 200) {
+          var json = resp.data
+          if (json.code === 1) {
+            this.monitorTypeItems = json.data
+          }
+        } else {
+          this.$message({
+            message: '获取分组信息失败',
+            type: 'error'
+          })
+        }
+      })
+    },
     current_hostType (selVal) {
       this.currentHostType = selVal
     },
@@ -377,7 +528,17 @@ export default {
       })
     }
   },
-  actions: {
+  mounted () {
+    if (this.timer) {
+      clearInterval(this.timer)
+    } else {
+      this.timer = setInterval(() => {
+        this.showInfo()
+      }, 1000 * 60 * 2)
+    }
+  },
+  destroyed () {
+    clearInterval(this.timer)
   },
   components: { Pagination }
 }
@@ -401,5 +562,10 @@ export default {
 }
 /deep/.el-button {
   margin-left: 10px;
+}
+/deep/.el-tag--dark {
+  border-radius: 1px;
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.2),
+    0 1px 5px 0 rgba(0, 0, 0, 0.12);
 }
 </style>
