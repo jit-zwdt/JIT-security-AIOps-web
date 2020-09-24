5430d40ba7eb6907d9710803ce6f88466b1694ea
jian_liu_ggzw <1115218686@qq.com>
2020-09-24 14:18:05 +0800

    更改内容：1、修改 更改人： jian_liu 更改时间：2020年9月24日10点31分

--- src/views/monitorManager/monitoryHardware/monitorIndex.vue
+++ src/views/monitorManager/monitoryHardware/monitorIndex.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-row :gutter="15">
-      <el-col :span="4">
+      <!-- <el-col :span="4">
         <div class="grid-left">
           <div>
             <el-input placeholder="分组名搜索" v-model="groupName" class="input-with-select" size="mini">
@@ -18,8 +18,8 @@
             >{{item.name}}</li>
           </ul>
         </div>
-      </el-col>
-      <el-col :span="20">
+      </el-col> -->
+      <el-col :span="24">
         <div class="grid-content">
           <el-row :gutter="15">
             <el-col :span="24">
