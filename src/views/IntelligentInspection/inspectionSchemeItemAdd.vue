6f6935a5e53c604838df556b4af14d5d38f3681f
jian_liu_ggzw <1115218686@qq.com>
2020-10-16 10:07:41 +0800

    更改内容：1、修改 更改人： jian_liu 更改时间：2020年10月10日10点07分

---
+++ src/views/IntelligentInspection/inspectionSchemeItemAdd.vue
@@ -0,0 +1,272 @@
+<template>
+  <el-dialog
+    @opened="openDialog"
+    :width="dialogWidth"
+    :title="title"
+    :visible.sync="addItemDialog"
+    :show-close="false"
+    :close-on-click-modal="false"
+    append-to-body
+    :close-on-press-escape="false"
+  >
+    <div>
+      <el-select
+        v-model="tophostid"
+        placeholder="请选择主机"
+        @change="chooseHost"
+      >
+        <el-option
+          v-for="item in nameOptions"
+          :key="item.id"
+          :label="item.objectName"
+          :value="item.hostId"
+        ></el-option>
+      </el-select>
+    </div>
+    <el-table
+      :data="
+        forShowData.slice(
+          (currentInsidePage - 1) * pageSize,
+          currentInsidePage * pageSize
+        )
+      "
+      v-loading="loading"
+      border
+      style="width: 100%"
+      :row-style="tableRowStyle"
+      :header-cell-style="tableHeaderColor"
+      ref="selectionTable"
+      tooltip-effect="dark"
+      :row-key="getRowKey"
+      @selection-change="handleSelectionChange"
+    >
+      <el-table-column
+        type="selection"
+        min-width="5%"
+        :reserve-selection="true"
+      ></el-table-column>
+      <el-table-column
+        label="triggerid"
+        prop="triggerid"
+        :resizable="false"
+        v-if="show"
+      ></el-table-column>
+      <el-table-column
+        label="触发器名称"
+        prop="description"
+        min-width="70%"
+      ></el-table-column>
+      <el-table-column
+        align="center"
+        label="启用监控"
+        min-width="10%"
+        :resizable="false"
+      >
+        <template slot-scope="scope">
+          <div v-html="statusformat(scope.row)"></div>
+        </template>
+      </el-table-column>
+      <el-table-column label="严重性" min-width="10%">
+        <template slot-scope="scope">
+          {{ valuetypeformatter(scope.row) }}
+        </template>
+      </el-table-column>
+    </el-table>
+    <div class="block" style="margin-top: 15px">
+      <el-pagination
+        align="left"
+        @size-change="handleSizeInsideChange"
+        @current-change="handleCurrentInsideChange"
+        :current-page="currentInsidePage"
+        :page-sizes="[10, 30, 50, 100]"
+        :page-size="pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="forShowData.length"
+      ></el-pagination>
+    </div>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="closefrom()">取消选择</el-button>
+      <el-button type="primary" @click="rightChose">确定选择</el-button>
+    </div>
+  </el-dialog>
+</template>
+<script>
+export default {
+  props: {
+    addItemDialog: Boolean,
+    dialogWidth: {
+      type: String,
+      default: '60rem'
+    },
+    title: {
+      type: String,
+      default: '监控项'
+    },
+    labelWidth: {
+      type: String,
+      default: '180px'
+    }
+  },
+  data () {
+    return {
+      tophostid: '',
+      loading: false,
+      forShowData: [],
+      nameOptions: [],
+      show: false,
+      currentPage: 1, // 当前页码
+      currentInsidePage: 1, // 监控项页码
+      currentGraphPage: 1, // 图形列表页码
+      total: 20, // 总条数
+      pageSize: 10, // 每页的数据条数
+      selectionData: []
+    }
+  },
+  methods: {
+    // 修改table tr行的背景色
+    tableRowStyle ({ row, column, rowIndex, columnIndex }) {
+    },
+    // 修改table header的背景色
+    tableHeaderColor ({ row, column, rowIndex, columnIndex }) {
+      if (rowIndex === 0) {
+        return 'background-color: #0086f1;color: #FFFFFF;font-weight: 500;font-size:15px'
+      }
+    },
+    openDialog () {
+      this.showInfo()
+    },
+    closefrom () {
+      this.clearform()
+      this.$emit('close')
+    },
+    clearform () {
+      this.forShowData = []
+      this.tophostid = ''
+      this.selectionData = []
+      this.$refs.selectionTable.clearSelection()
+    },
+    showInfo () {
+      const param = new URLSearchParams()
+      param.append('id', '')
+      this.axios.post(this.$api.inspectionManager.inspectionGetHostInfo, param).then((resp) => {
+        if (resp.status === 200) {
+          var json = resp.data
+          if (json.code === 1) {
+            this.nameOptions = json.data
+          }
+        } else {
+          this.$message({
+            message: '查询失败',
+            type: 'error'
+          })
+          this.$emit('error')
+        }
+      })
+    },
+    chooseHost (value) {
+      this.forShowData = []
+      this.loading = true
+      const region = {
+        hostId: value
+      }
+      this.axios.post(this.$api.monitorManager.findByCondition, region).then((resp) => {
+        if (resp.status === 200) {
+          var json = resp.data
+          if (json.code === 1) {
+            this.forShowData = json.data
+            this.currentPage = 1
+          }
+        } else {
+          this.$message({
+            message: '查询失败',
+            type: 'error'
+          })
+        }
+        this.loading = false
+      })
+    },
+    rightChose () {
+      this.$emit('success', this.selectionData)
+      this.clearform()
+    },
+    getRowKey (row) {
+      return row.itemid
+    },
+    handleSelectionChange (val) {
+      if (val !== null) {
+        val.forEach(element => {
+          element.hostid = this.tophostid
+        })
+      }
+      this.selectionData = val
+    },
+    valuetypeformatter (row) {
+      var name = ''
+      switch (row.priority) {
+        case 1:
+          name = '信息'
+          break
+        case 2:
+          name = '警告'
+          break
+        case 3:
+          name = '一般严重'
+          break
+        case 4:
+          name = '严重'
+          break
+        case 5:
+          name = '灾难'
+          break
+        default:
+          name = '未分类'
+          break
+      }
+      return name
+    },
+    statusformat (row) {
+      var name = ''
+      switch (row.status) {
+        case 0:
+          name = '启用'
+          break
+        default:
+          name = '<span style="color:red">停用</span>'
+          break
+      }
+      return name
+    },
+    handleSizeInsideChange (val) {
+      this.currentInsidePage = 1
+      this.pageSize = val
+    },
+    handleCurrentInsideChange (val) {
+      this.currentInsidePage = val
+    }
+  },
+  components: {}
+}
+</script>
+<style lang="scss" scoped>
+.fromadd {
+  width: 100%;
+}
+.queryleft {
+  float: left;
+}
+.queryright {
+  float: right;
+}
+/deep/ .el-table__header {
+  width: 100% !important;
+}
+/deep/ .el-table {
+  min-height: 1rem !important;
+}
+.changeColor {
+  color: green;
+}
+.redchangeColor {
+  color: red;
+}
+</style>
