c541c22cfc21fa0c29b4d2791da10b4310142c44
任爽 <26572093451@qq.com>
2020-10-19 12:51:03 +0800

    首页顶部修改

--- src/views/layout/BodyTop.vue
+++ src/views/layout/BodyTop.vue
@@ -9,25 +9,6 @@
           <i class="el-icon-bell"></i>
         </el-badge>
       </span>
-      <el-dropdown>
-        <span class="body-top-btn">
-          <i class="el-icon-bell"></i>
-        </span>
-        <el-dropdown-menu slot="dropdown">
-          <div style="padding: 10px 20px;text-align: center;">
-            导航类型：
-            <el-button-group>
-              <el-button
-                :type="system.navType === item.value ? 'primary' : ''"
-                size="mini"
-                v-for="item in GlobalCfg.systemNavType"
-                :key="item.value"
-                @click="$store.commit('NAV_TYPE_TOGGLE', item.value)"
-              >{{ item.label }}</el-button>
-            </el-button-group>
-          </div>
-        </el-dropdown-menu>
-      </el-dropdown>
       <span class="body-top-btn" @click="screenFullToggle">
         <i class="fa fa-arrows-alt"></i>
       </span>
