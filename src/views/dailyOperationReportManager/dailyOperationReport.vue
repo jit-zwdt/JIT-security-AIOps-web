1edd51215721d50b486d2be00038d40bff32e683
苗增新 <mzxdyx123@163.com>
2020-11-27 17:02:20 +0800

    Author:zengxin_miao Date:2020.11.27 Change： add zone

--- src/views/dailyOperationReportManager/dailyOperationReport.vue
+++ src/views/dailyOperationReportManager/dailyOperationReport.vue
@@ -97,7 +97,7 @@ export default {
       this.axios.get(this.$api.dailyOperationReportManager.getDailyOperationReport).then(resp => {
         if (resp.status === 200) {
           var json = resp.data
-          console.log(json.code)
+          console.log(json)
           if (json.code === 1) {
             console.log('success')
             this.dailyOperationsData = json.data
